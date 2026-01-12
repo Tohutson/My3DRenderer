26c66a325b9c9aa9876f25e3f4485e8a5fb6a4d2
Tohutson <tohutson@example.com>
2026-01-12 17:16:15 -0500

    Setup GUI

--- src/main/java/com/treyhutson/DemoViewer.java
+++ src/main/java/com/treyhutson/DemoViewer.java
@@ -1,7 +1,35 @@
 package com.treyhutson;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class DemoViewer {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        JFrame frame = new JFrame("Demo Viewer");
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
+        // slider for horizontal rotation
+        JSlider headingSlider = new JSlider(0, 360, 180);
+        pane.add(headingSlider, BorderLayout.SOUTH);
+
+        // slider for vertical rotation
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        pane.add(pitchSlider, BorderLayout.EAST);
+
+        // panel to display render results
+        JPanel renderPanel = new JPanel() {
+            public void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setColor(Color.BLACK);
+                g2.fillRect(0, 0, getWidth(), getHeight());
+
+                // rendering will be here
+            }
+        };
+        pane.add(renderPanel, BorderLayout.CENTER);
+
+        frame.setSize(400, 400);
+        frame.setVisible(true);
     }
 }
