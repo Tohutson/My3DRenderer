b47ee480e0689bdf77bec02ab68efc92945540dc
Tohutson <tohutson@example.com>
2026-01-13 20:55:11 -0500

    add basic rasterization

--- src/main/java/com/treyhutson/DemoViewer.java
+++ src/main/java/com/treyhutson/DemoViewer.java
@@ -1,7 +1,14 @@
 package com.treyhutson;
 
+import com.treyhutson.Math.Matrix3;
+import com.treyhutson.Models.Triangle;
+import com.treyhutson.Models.Vertex;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class DemoViewer {
     public static void main(String[] args) {
@@ -25,11 +32,65 @@ public class DemoViewer {
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
                 // rendering will be here
+                ArrayList<Triangle> tris = new ArrayList<>();
+                tris.add(new Triangle(new Vertex(100, 100, 100), new Vertex(-100, -100, 100), new Vertex(-100, 100, -100), Color.WHITE));
+                tris.add(new Triangle(new Vertex(100, 100, 100), new Vertex(-100, -100, 100), new Vertex(100, -100, -100), Color.RED));
+                tris.add(new Triangle(new Vertex(-100, 100, -100), new Vertex(100, -100, -100), new Vertex(100, 100, 100), Color.GREEN));
+                tris.add(new Triangle(new Vertex(-100, 100, -100), new Vertex(100, -100, -100), new Vertex(-100, -100, 100), Color.BLUE));
+                double heading = Math.toRadians(headingSlider.getValue());
+                Matrix3 headingTransform = new Matrix3(new double[]{Math.cos(heading), 0, -Math.sin(heading), 0, 1, 0, Math.sin(heading), 0, Math.cos(heading)});
+
+                double pitch = Math.toRadians(pitchSlider.getValue());
+                Matrix3 pitchTransform = new Matrix3(new double[]{1, 0, 0, 0, Math.cos(pitch), Math.sin(pitch), 0, -Math.sin(pitch), Math.cos(pitch)});
+
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
+
+                BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+                for (Triangle t : tris) {
+                    Vertex v1 = transform.transform(t.getVertex1());
+                    Vertex v2 = transform.transform(t.getVertex2());
+                    Vertex v3 = transform.transform(t.getVertex3());
+
+                    // since we are not using Graphics2D anymore,
+                    // we have to do translation manually
+                    v1.setX(v1.getX() + getWidth() / 2);
+                    v1.setY(v1.getY() + getWidth() / 2);
+                    v2.setX(v2.getX() + getWidth() / 2);
+                    v2.setY(v2.getY() + getWidth() / 2);
+                    v3.setX(v3.getX() + getWidth() / 2);
+                    v3.setY(v3.getY() + getWidth() / 2);
+                    
+                    // compute rectangular bounds for triangle
+                    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.getX(), Math.min(v2.getX(), v3.getX()))));
+                    int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.getX(), Math.max(v2.getX(), v3.getX()))));
+                    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.getY(), Math.min(v2.getY(), v3.getY()))));
+                    int maxY = (int) Math.min(img.getHeight() - 1, Math.floor(Math.max(v1.getY(), Math.max(v2.getY(), v3.getY()))));
+
+                    double triangleArea = (v1.getY() - v3.getY()) * (v2.getX() - v3.getX()) + (v2.getY() - v3.getY()) * (v3.getX() - v1.getX());
+
+                    for (int y = minY; y <= maxY; y++) {
+                        for (int x = minX; x <= maxX; x++) {
+                            double b1 = ((y - v3.getY()) * (v2.getX() - v3.getX()) + (v2.getY() - v3.getY()) * (v3.getX() - x)) / triangleArea;
+                            double b2 = ((y - v1.getY()) * (v3.getX() - v1.getX()) + (v3.getY() - v1.getY()) * (v1.getX() - x)) / triangleArea;
+                            double b3 = ((y - v2.getY()) * (v1.getX() - v2.getX()) + (v1.getY() - v2.getY()) * (v2.getX() - x)) / triangleArea;
+                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                                img.setRGB(x, y, t.getColor().getRGB());
+                            }
+                        }
+                    }
+
+                }
+
+                g2.drawImage(img, 0, 0, null);
             }
+
         };
         pane.add(renderPanel, BorderLayout.CENTER);
 
         frame.setSize(400, 400);
         frame.setVisible(true);
+        headingSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e -> renderPanel.repaint());
     }
 }
